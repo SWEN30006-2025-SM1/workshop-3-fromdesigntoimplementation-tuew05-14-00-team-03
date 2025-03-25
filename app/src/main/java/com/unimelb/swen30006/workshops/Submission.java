e5e85b7b97571daa3321b20c7e1f1c8c367d5c49
casey <cwattcalder@student.unimelb.edu.au>
2025-03-25 15:59:47 +1100

    began student

--- app/src/main/java/com/unimelb/swen30006/workshops/Submission.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Submission.java
@@ -2,11 +2,13 @@ package com.unimelb.swen30006.workshops;
 
 // Sample private submission class to be replaced by your implementation
 class Submission {
+
+    private File data;
+    private String note;
+
     // Return two fake files
-    public File[] includedFiles(){
-        File[] files = new File[2];
-        files[0] = new File();
-        files[1] = new File();
-        return files;
+    public Submission(File data, String note) {
+        this.note = note;
+        this.data = data;
     }
 }
